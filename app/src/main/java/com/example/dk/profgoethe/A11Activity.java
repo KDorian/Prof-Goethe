3278991c6dccf024e012280a1c46c0fddc3a11c3
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-06-02 20:58:38 +0200

    Add new MainActivity layout

---
+++ app/src/main/java/com/example/dk/profgoethe/A11Activity.java
@@ -0,0 +1,9 @@
+package com.example.dk.profgoethe;
+
+import android.app.Activity;
+
+/**
+ * Created by DK on 2016-06-02.
+ */
+public class A11Activity extends Activity {
+}
--- app/src/main/java/com/example/dk/profgoethe/MainActivity.java
+++ app/src/main/java/com/example/dk/profgoethe/MainActivity.java
@@ -1,52 +1,40 @@
 package com.example.dk.profgoethe;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    String[] choices;
+    ListView choicesListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Resources resources = getResources();
+        choices = resources.getStringArray(R.array.choices_array);
+        choicesListView = (ListView) findViewById(R.id.listView);
+        choicesListView.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, choices));
+        choicesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        startActivity(new Intent(MainActivity.this, A11Activity.class));
+                        break;
+                }
+
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
