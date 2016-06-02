f4480c5bb8a7a9d241695ad5fc22f13533817687
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-06-02 22:46:35 +0200

    Add drawer to A11

---
+++ app/src/main/java/com/example/dk/profgoethe/A11Fragment.java
@@ -0,0 +1,31 @@
+package com.example.dk.profgoethe;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by DK on 2016-06-02.
+ */
+public class A11Fragment extends Fragment {
+
+
+    public A11Fragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_a11, container, false);
+    }
+
+
+}
