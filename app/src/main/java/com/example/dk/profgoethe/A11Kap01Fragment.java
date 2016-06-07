d3e1871868a16f6bdabc31146e06bf5ff948258b
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-06-07 13:21:23 +0200

    Add multicolor TextView to layout A1.1. Kapitel01

--- app/src/main/java/com/example/dk/profgoethe/A11Kap01Fragment.java
+++ app/src/main/java/com/example/dk/profgoethe/A11Kap01Fragment.java
@@ -3,11 +3,15 @@ package com.example.dk.profgoethe;
 /**
  * Created by DK on 2016-06-04.
  */
+
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 
 /**
  * Created by DK on 2016-06-02.
@@ -27,7 +31,16 @@ public class A11Kap01Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_a11_kap01, container, false);
+        // -- inflate the layout for this fragment
+        View myInflatedView = inflater.inflate(R.layout.fragment_a11_kap01, container,false);
+
+        // Set the Text to try this out
+        TextView t = (TextView) myInflatedView.findViewById(R.id.textView89);
+        String first = "komm";
+        String next = "<font color='#EE0000'>e</font>";
+        t.setText(Html.fromHtml(first + next));
+
+        return myInflatedView;
     }
 
 
