a0c677dd6d5baa480fad48a94041300c51aa2cde
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-06-09 13:27:54 +0200

    Add layout & class of A1.1. Kapitel04

---
+++ app/src/main/java/com/example/dk/profgoethe/A11Kap04Fragment.java
@@ -0,0 +1,32 @@
+package com.example.dk.profgoethe;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by DK on 2016-06-09.
+ */
+public class A11Kap04Fragment extends Fragment {
+
+
+
+        public A11Kap04Fragment() {
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_a11_kap04, container, false);
+        }
+
+
+    }
