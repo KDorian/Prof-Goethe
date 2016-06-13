8c29bd700677c35044df1f74284d7d461c5ad016
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-06-13 23:08:06 +0200

    Add special german letters & multicolored TextViews to A1.1. Kapitel07 layout

--- app/src/main/java/com/example/dk/profgoethe/A11Kap07Fragment.java
+++ app/src/main/java/com/example/dk/profgoethe/A11Kap07Fragment.java
@@ -2,9 +2,11 @@ package com.example.dk.profgoethe;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by DK on 2016-06-12.
@@ -24,8 +26,22 @@ public class A11Kap07Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_a11_kap07, container, false);
+        // -- inflate the layout for this fragment
+        View myInflatedView = inflater.inflate(R.layout.fragment_a11_kap07, container, false);
+
+        // Set the Text to try this out
+        TextView t = (TextView) myInflatedView.findViewById(R.id.textView381);
+        String first = "<font color='#EE0000'>kann</font>";
+        t.setText(Html.fromHtml(first));
+
+        TextView t1 = (TextView) myInflatedView.findViewById(R.id.textView382);
+        String first1 = "<font color='#EE0000'>kannst</font>";
+        t1.setText(Html.fromHtml(first1));
+
+        TextView t2 = (TextView) myInflatedView.findViewById(R.id.textView383);
+        String first2 = "<font color='#EE0000'>kann</font>";
+        t2.setText(Html.fromHtml(first2));
+
+        return myInflatedView;
     }
-
-
 }
