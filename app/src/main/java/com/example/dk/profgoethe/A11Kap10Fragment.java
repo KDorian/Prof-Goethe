8e15583fd4e2dd43a9f61ed4babdddf17c60cc83
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-06-18 18:12:03 +0200

    Add all strings to A1.1. Kapitel10 layout

--- app/src/main/java/com/example/dk/profgoethe/A11Kap10Fragment.java
+++ app/src/main/java/com/example/dk/profgoethe/A11Kap10Fragment.java
@@ -1,7 +1,31 @@
 package com.example.dk.profgoethe;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by DK on 2016-06-18.
  */
-public class A11Kap10Fragment {
+public class A11Kap10Fragment extends Fragment {
+
+
+    public A11Kap10Fragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_a11_kap10, container, false);
+    }
+
+
 }
