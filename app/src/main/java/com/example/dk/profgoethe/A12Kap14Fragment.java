aaf3096bb448abcbbba19194927f15e12da52632
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-06-22 00:02:40 +0200

    Add multicolored TextViews to A1.2. Kapitel14 layout

--- app/src/main/java/com/example/dk/profgoethe/A12Kap14Fragment.java
+++ app/src/main/java/com/example/dk/profgoethe/A12Kap14Fragment.java
@@ -2,9 +2,11 @@ package com.example.dk.profgoethe;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by DK on 2016-06-21.
@@ -24,7 +26,19 @@ public class A12Kap14Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_a12_kap14, container, false);
+        // -- inflate the layout for this fragment
+        View myInflatedView = inflater.inflate(R.layout.fragment_a12_kap14, container, false);
+
+        // Set the Text to try this out
+        TextView t = (TextView) myInflatedView.findViewById(R.id.textView754);
+        String first = "sein<font color='#EE0000'>en</font>";
+        t.setText(Html.fromHtml(first));
+
+        TextView t1 = (TextView) myInflatedView.findViewById(R.id.textView759);
+        String first1 = "ihr<font color='#EE0000'>en</font>";
+        t1.setText(Html.fromHtml(first1));
+
+        return myInflatedView;
     }
 
 
