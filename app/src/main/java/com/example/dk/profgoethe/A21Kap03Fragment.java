421171c39477434ecfd3c4c2ac3b1d2520bbc9a5
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-07-11 19:36:27 +0200

    Add multicolored TextViews to A2.1. Kapitel03 layout

--- app/src/main/java/com/example/dk/profgoethe/A21Kap03Fragment.java
+++ app/src/main/java/com/example/dk/profgoethe/A21Kap03Fragment.java
@@ -2,9 +2,11 @@ package com.example.dk.profgoethe;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by DK on 2016-07-11.
@@ -24,7 +26,19 @@ public class A21Kap03Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_red, container, false);
+        // -- inflate the layout for this fragment
+        View myInflatedView = inflater.inflate(R.layout.fragment_a21_kap03, container, false);
+
+        // Set the Text to try this out
+        TextView t = (TextView) myInflatedView.findViewById(R.id.textView1884);
+        String first = "wander-n + -er -> der <font color='#EE0000'>Wander<b>er</b></font>";
+        t.setText(Html.fromHtml(first));
+
+        TextView t1 = (TextView) myInflatedView.findViewById(R.id.textView1888);
+        String first1 = "erfahr-en + -ung -> die <font color='#EE0000'>Erfahr<b>ung</b></font>";
+        t1.setText(Html.fromHtml(first1));
+
+        return myInflatedView;
     }
 
 
