955df1c882a3626b1b5f679a69fef126c02931e2
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-07-18 21:22:57 +0200

    Add colored TextView to A2.1. Kapitel10 layout

--- app/src/main/java/com/example/dk/profgoethe/A21Kap10Fragment.java
+++ app/src/main/java/com/example/dk/profgoethe/A21Kap10Fragment.java
@@ -2,9 +2,11 @@ package com.example.dk.profgoethe;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by DK on 2016-07-18.
@@ -24,7 +26,15 @@ public class A21Kap10Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_a21_kap10, container, false);
+        // -- inflate the layout for this fragment
+        View myInflatedView = inflater.inflate(R.layout.fragment_a21_kap10, container, false);
+
+        // Set the Text to try this out
+        TextView t = (TextView) myInflatedView.findViewById(R.id.textView2227);
+        String first = "Ich hoffe, <font color='#EE0000'>dass</font> sie Pommes <font color='#EE0000'>haben</font>.";
+        t.setText(Html.fromHtml(first));
+
+        return myInflatedView;
     }
 
 
