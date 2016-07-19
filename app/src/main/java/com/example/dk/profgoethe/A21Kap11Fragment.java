83307f23bb01a12d24441d576e9ca0aba4ad7861
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-07-19 22:43:10 +0200

    Add multicolored TextViews to A2.1. Kapitel11 layout

--- app/src/main/java/com/example/dk/profgoethe/A21Kap11Fragment.java
+++ app/src/main/java/com/example/dk/profgoethe/A21Kap11Fragment.java
@@ -2,9 +2,11 @@ package com.example.dk.profgoethe;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by DK on 2016-07-19.
@@ -24,7 +26,39 @@ public class A21Kap11Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_a21_kap011, container, false);
+        // -- inflate the layout for this fragment
+        View myInflatedView = inflater.inflate(R.layout.fragment_a21_kap11, container, false);
+
+        // Set the Text to try this out
+        TextView t = (TextView) myInflatedView.findViewById(R.id.textView2293);
+        String first = "Aber <font color='#EE0000'>ich</font> f&#252;hle <font color='#EE0000'>mich</font> troztdem prima.";
+        t.setText(Html.fromHtml(first));
+
+        TextView t1 = (TextView) myInflatedView.findViewById(R.id.textView2300);
+        String first1 = "<font color='#EE0000'>mich</font>";
+        t1.setText(Html.fromHtml(first1));
+
+        TextView t2 = (TextView) myInflatedView.findViewById(R.id.textView2301);
+        String first2 = "<font color='#EE0000'>dich</font>";
+        t2.setText(Html.fromHtml(first2));
+
+        TextView t3 = (TextView) myInflatedView.findViewById(R.id.textView2302);
+        String first3 = "<font color='#EE0000'>sich</font>";
+        t3.setText(Html.fromHtml(first3));
+
+        TextView t4 = (TextView) myInflatedView.findViewById(R.id.textView2303);
+        String first4 = "<font color='#EE0000'>uns</font>";
+        t4.setText(Html.fromHtml(first4));
+
+        TextView t5 = (TextView) myInflatedView.findViewById(R.id.textView2304);
+        String first5 = "<font color='#EE0000'>euch</font>";
+        t5.setText(Html.fromHtml(first5));
+
+        TextView t6 = (TextView) myInflatedView.findViewById(R.id.textView2305);
+        String first6 = "<font color='#EE0000'>sich</font>";
+        t6.setText(Html.fromHtml(first6));
+
+        return myInflatedView;
     }
 
 
