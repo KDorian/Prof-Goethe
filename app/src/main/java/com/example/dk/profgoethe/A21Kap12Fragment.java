8df0eb5290f3661865ccc78eb5442376c2948e10
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-07-20 20:32:42 +0200

    Add multicolored TextViews to A2.1. Kapitel12 layout

--- app/src/main/java/com/example/dk/profgoethe/A21Kap12Fragment.java
+++ app/src/main/java/com/example/dk/profgoethe/A21Kap12Fragment.java
@@ -2,9 +2,11 @@ package com.example.dk.profgoethe;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by DK on 2016-07-20.
@@ -24,7 +26,27 @@ public class A21Kap12Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_a21_kap12, container, false);
+        // -- inflate the layout for this fragment
+        View myInflatedView = inflater.inflate(R.layout.fragment_a21_kap12, container, false);
+
+        // Set the Text to try this out
+        TextView t = (TextView) myInflatedView.findViewById(R.id.textView2351);
+        String first = "<font color='#EE0000'>Wenn</font> es warm ist,";
+        t.setText(Html.fromHtml(first));
+
+        TextView t1 = (TextView) myInflatedView.findViewById(R.id.textView2352);
+        String first1 = "<font color='#EE0000'>Wenn</font> es schnell gehen muss,";
+        t1.setText(Html.fromHtml(first1));
+
+        TextView t2 = (TextView) myInflatedView.findViewById(R.id.textView2359);
+        String first2 = "<font color='#EE0000'>wenn</font> es warm ist.";
+        t2.setText(Html.fromHtml(first2));
+
+        TextView t3 = (TextView) myInflatedView.findViewById(R.id.textView2360);
+        String first3 = "<font color='#EE0000'>wenn</font> es schnell gehen muss.";
+        t3.setText(Html.fromHtml(first3));
+
+        return myInflatedView;
     }
 
 
