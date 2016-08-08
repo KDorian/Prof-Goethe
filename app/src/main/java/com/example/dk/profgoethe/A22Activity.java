6a1d384b0da4de4597080594c84665ffeef2a72c
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-08-08 20:01:29 +0200

    Add A2.2. Kap20 layout & class

--- app/src/main/java/com/example/dk/profgoethe/A22Activity.java
+++ app/src/main/java/com/example/dk/profgoethe/A22Activity.java
@@ -84,6 +84,9 @@ public class A22Activity extends ActionBarActivity {
                     case 7:  //A2.2. Kapitel19
                         fragment = new A22Kap19Fragment();
                         break;
+                    case 8:  //A2.2. Kapitel20
+                        fragment = new A22Kap20Fragment();
+                        break;
                 }
                 fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
                 setTitle(mDrawerOptionLabels[position]);
