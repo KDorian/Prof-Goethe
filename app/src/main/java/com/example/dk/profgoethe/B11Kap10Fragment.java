4cae696f7e97ebdc6ecc839c68b7a7cbf5ff7776
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-08-22 21:50:10 +0200

    Add multicolored TextViews to B1.1. Kap10 layout

--- app/src/main/java/com/example/dk/profgoethe/B11Kap10Fragment.java
+++ app/src/main/java/com/example/dk/profgoethe/B11Kap10Fragment.java
@@ -2,9 +2,11 @@ package com.example.dk.profgoethe;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by DK on 22.08.2016.
@@ -24,7 +26,19 @@ public class B11Kap10Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_red, container, false);
+        // -- inflate the layout for this fragment
+        View myInflatedView = inflater.inflate(R.layout.fragment_b11_kap10, container, false);
+
+        // Set the Text to try this out
+        TextView t = (TextView) myInflatedView.findViewById(R.id.textView3664);
+        String first = "<font color='#EE0000'>H&#228;tten</font> wir doch die erste Wohnung <font color='#EE0000'>genommen</font>!";
+        t.setText(Html.fromHtml(first));
+
+        TextView t1 = (TextView) myInflatedView.findViewById(R.id.textView3677);
+        String first1 = "<font color='#EE0000'>W&#228;re</font> sie doch nur rechtzeitig <font color='#EE0000'>losgegangen</font>!";
+        t1.setText(Html.fromHtml(first1));
+
+        return myInflatedView;
     }
 
 
