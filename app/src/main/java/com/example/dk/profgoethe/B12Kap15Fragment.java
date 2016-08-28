da4b09470887ca8adc8ade8923b1568fdb38f215
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-08-29 00:17:52 +0200

    Add B1.2. Kap16 layout & class

--- app/src/main/java/com/example/dk/profgoethe/B12Kap15Fragment.java
+++ app/src/main/java/com/example/dk/profgoethe/B12Kap15Fragment.java
@@ -2,9 +2,11 @@ package com.example.dk.profgoethe;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by DK on 28.08.2016.
@@ -24,7 +26,19 @@ public class B12Kap15Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_red, container, false);
+        // -- inflate the layout for this fragment
+        View myInflatedView = inflater.inflate(R.layout.fragment_b12_kap15, container, false);
+
+        // Set the Text to try this out
+        TextView t = (TextView) myInflatedView.findViewById(R.id.textView4012);
+        String first = "Ich spreche <font color='#EE0000'>sowohl</font> Deutsch <font color='#EE0000'>als auch</font> Spanisch.";
+        t.setText(Html.fromHtml(first));
+
+        TextView t1 = (TextView) myInflatedView.findViewById(R.id.textView4013);
+        String first1 = "Ich spreche <font color='#EE0000'>nicht nur</font> Deutsch, <font color='#EE0000'>sondern auch</font> Spanisch.";
+        t1.setText(Html.fromHtml(first1));
+
+        return myInflatedView;
     }
 
 
