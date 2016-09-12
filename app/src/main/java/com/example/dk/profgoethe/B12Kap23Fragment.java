0b8a1e4070c8116b007ade511f6a6e0a13191b0f
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-09-12 23:34:43 +0200

    Start changing class of B1.2. Kap23 layout

--- app/src/main/java/com/example/dk/profgoethe/B12Kap23Fragment.java
+++ app/src/main/java/com/example/dk/profgoethe/B12Kap23Fragment.java
@@ -2,9 +2,11 @@ package com.example.dk.profgoethe;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by DK on 05.09.2016.
@@ -24,7 +26,15 @@ public class B12Kap23Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_red, container, false);
+        // -- inflate the layout for this fragment
+        View myInflatedView = inflater.inflate(R.layout.fragment_b12_kap21, container, false);
+
+        // Set the Text to try this out
+        TextView t = (TextView) myInflatedView.findViewById(R.id.textView4269);
+        String first = "<font color='#EE0000'>um &#8230; herum</font> + Akkusativ";
+        t.setText(Html.fromHtml(first));
+
+        return myInflatedView;
     }
 
 
