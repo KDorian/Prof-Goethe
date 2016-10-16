752668228556c98dbaa34e2237ab4e3abca744ae
Dorian Kucharski <dorian.kucharski@gmail.com>
2016-10-16 23:15:39 +0200

    Add B1.3. Drawer, Topics Page & B1.3. Fragment

---
+++ app/src/main/java/com/example/dk/profgoethe/B13Activity.java
@@ -0,0 +1,118 @@
+package com.example.dk.profgoethe;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Brodull on 16.10.2016.
+ */
+
+public class B13Activity extends ActionBarActivity {
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerListView;
+    private RelativeLayout mDrawerRelativeLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    String[] mDrawerOptionLabels;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer_b13);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerListView = (ListView) findViewById(R.id.left_drawer);
+        mDrawerRelativeLayout = (RelativeLayout) findViewById(R.id.left_drawer_layout);
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        );
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        Resources resources = getResources();
+        mDrawerOptionLabels = resources.getStringArray(R.array.sliding_drawer_array_b13);
+        ArrayAdapter<String> drawerAdapter = new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mDrawerOptionLabels);
+
+        mDrawerListView.setAdapter(drawerAdapter);
+        mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                FragmentManager fragmentManager = getFragmentManager();
+                Fragment fragment = new B13Fragment();
+                switch (position){ // note this is the order in the TiteList which is constant!
+                    case 0:  //B1.1.
+                        fragment = new B13Fragment();
+                        break;
+                    case 1:  //B1.1. Kap01
+                        fragment = new B13Kap01Fragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+                setTitle(mDrawerOptionLabels[position]);
+                mDrawerListView.setItemChecked(position, true);
+                mDrawerLayout.closeDrawer(mDrawerRelativeLayout);
+            }
+        });
+
+        if (savedInstanceState == null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            Fragment fragment = new B13Fragment();
+            fragment = new B13Fragment();
+            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+            setTitle(mDrawerOptionLabels[0]);
+        }
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_drawer_navigation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/dk/profgoethe/B13Fragment.java
@@ -0,0 +1,32 @@
+package com.example.dk.profgoethe;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Brodull on 16.10.2016.
+ */
+
+public class B13Fragment extends Fragment {
+
+
+    public B13Fragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_b13, container, false);
+    }
+
+
+}
